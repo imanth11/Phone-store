afdbf3555b6a4a8d6f5285683320a3099a235d79
imanth11 <imanhunter44@gmail.com>
2025-10-16 02:27:06 -0700

    login

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -4,25 +4,29 @@ import { emit } from "process";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import { signToken } from "@/lib/jwt";
+import { User } from "../models/user";
+import { connectDB } from "@/lib/mongodb";
 
-interface Iuser{
-    id:number,
-    name:string,
-    email:string,
-    password:string
-}
+interface IUser {
+    _id: string;
+    name: string;
+    email: string;
+    password: string;
+  }
+  
 export async function POST(req:Request){
 
 
 try{
+
+    await connectDB();
     const {email , password}= await req.json();
 
 
-const filepath=path.join(process.cwd(),"src","user.json");
-const filedata=fs.readFileSync(filepath,"utf-8");
-const users=JSON.parse(filedata);
 
-const user=users.find((u:Iuser)=>u.email===email)
+
+const user = await User.findOne({ email });
+
 if(!user){
     return NextResponse.json(
 
@@ -38,15 +42,16 @@ if(!isvalid){
     )
 }
 
-const token = signToken({ id: user.id, email: user.email, name: user.name });
+const token = signToken({ id: user._id, email: user.email, name: user.name });
 
   // ✅ ذخیره در کوکی (امن)
-  const res = NextResponse.json({ success: true, message: "Logged in" ,user: { id: user.id, name: user.name, email: user.email },});
+  const res = NextResponse.json({ success: true, message: "Logged in" ,user: { id: user._id, name: user.name, email: user.email },});
   res.cookies.set("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "lax",
     path: "/",
+    maxAge: 60 * 60 * 24 * 7, 
   });
 
   return res;
--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export async function POST(req: NextRequest) {
+  const response = NextResponse.json({ success: true, message: "Logged out" });
 
+  response.cookies.set("token", "", {
+    path: "/",
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    maxAge: 0, // حذف کوکی
+  });
 
-export async function POST(){
-    const response=NextResponse.json({success:true});
-    response.cookies.set("token", "", {
-        path: "/",
-        httpOnly: true,
-        maxAge: 0,
-      });
-    
-      return response;
+  return response;
 }
